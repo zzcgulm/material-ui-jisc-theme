bbeb67056e3005d7cea8116c5ac2de2740738187
Leigh Morris <leigh.morris@jisc.ac.uk>
2020-03-04 14:57:02 +0000

    Demo effort at customizing breakpoints.

--- jisc-theme/src/App.js
+++ jisc-theme/src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 import JiscBanner from './components/JiscBanner'
+
+const theme = createMuiTheme({
+  breakpoints: {
+    // Define custom breakpoint values.
+    // These will apply to Material-UI components that use responsive
+    // breakpoints, such as `Grid` and `Hidden`. You can also use the
+    // theme breakpoint functions `up`, `down`, and `between` to create
+    // media queries for these breakpoints
+    values: {
+      xs: 0,
+      sm: 757,
+      md: 850,
+      lg: 990,
+      xl: 1280
+    }
+  }
+});
 
 function App() {
   return (
-    <header>
+    <MuiThemeProvider theme={theme}>
+      <header>
 
-      <JiscBanner />
+        <JiscBanner />
 
-    </header>
+      </header>
+    </MuiThemeProvider>
   );
 }
 
